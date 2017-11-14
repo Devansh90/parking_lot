4a4b9def72e871afaa4716e791e96bdf33482f96
Devansh Rusia <devansh.rusia@olacabs.com>
2017-11-14 12:37:39 +0530

    Commands & Model

---
+++ src/main/java/com/devansh/personal/constants/Constants.java
@@ -0,0 +1,11 @@
+package com.devansh.personal.constants;
+
+public class Constants {
+
+    public final class Commands {
+        public static final String create_parking_lot = "create_parking_lot";
+        public static final String park_car = "park";
+        public static final String car_departure = "leave";
+        public static final String lot_status = "status";
+    }
+}
