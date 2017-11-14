228ec2165c3f27b3eb777debe0c55aa91b3af634
Devansh Rusia <devansh.rusia@olacabs.com>
2017-11-14 15:00:49 +0530

    Comments Phase I

--- src/main/java/com/devansh/personal/PrimaryApp.java
+++ src/main/java/com/devansh/personal/PrimaryApp.java
@@ -11,23 +11,31 @@ public class PrimaryApp {
     ParkingLot[] parkingLotList;
 
     public static void main(String args[]) {
-        //Test command
-
+        //Scanner
         Scanner scanner = new Scanner(System.in).useDelimiter("\\s");
         PrimaryApp app = new PrimaryApp();
 
+        //TODO
+        //Check if args is there & is of type file
+
+        //Commands
         String command = scanner.next();
         while (!Constants.Commands.exit.equalsIgnoreCase(command)) {
             try {
+                //Execute Command
                 app.executeCommand(command, scanner);
             } catch (ParkingLotException e) {
+                //Display Exception if any
                 System.out.println(e.getMessage());
             }
+            //Get next command
             command = scanner.next();
         }
+        //Treat Message
         System.out.println("Thanks for using the system!");
     }
 
+    //To Execute every type of command
     private void executeCommand(final String command, final Scanner inputScanner) {
         switch (command) {
             case Constants.Commands.create_parking_lot:
@@ -39,6 +47,7 @@ public class PrimaryApp {
         }
     }
 
+    //Create new parking lot, throws designed exceptions
     private void createParkingLot(final String next) {
 
         Integer slotCount = 0;
@@ -46,7 +55,7 @@ public class PrimaryApp {
         try {
             slotCount = Integer.parseInt(next);
         } catch (NumberFormatException e) {
-            throw new ParkingLotException("Parking lot size not defined");
+            throw new ParkingLotException("Parking lot size not defined / unrecognizable");
         }
 
         if (parkingLotList != null) throw new ParkingLotException("Parking lot is already initialized!");
